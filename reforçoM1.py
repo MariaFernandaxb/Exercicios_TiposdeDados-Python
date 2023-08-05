3adfd349f36573731e286304e72a721a7d8c6a5a
nandabdev <nandab.dev@gmail.com>
2023-08-04 22:16:10 -0300

    monitoria

---
+++ reforçoM1.py
@@ -0,0 +1,70 @@
+'''tipos de dados'''
+# int = 1,2,3,500,2000
+# float = 1.5, 2.0, 500.2
+# strings = Maria, Isadora, Fernanda, Rafael, Sherlon
+# # variaveis = nada mais é que a caracteristica/definição de alguma
+# """
+# soma = 10+5
+# nome1 = 'Isadora'
+# nome2 = 'Maria'
+
+# #if e else
+# #if = se
+# #else = se não
+
+# numero1 = 10
+# numero2 = 5
+
+# if numero1 > numero2:
+#     print('Numero 1 é maior que o número 2')
+# else:
+#     print('Numero 2 é maior que o número 1')
+# """ 
+# """while = enquanto
+# idade = int(input('Digite sua idade: '))
+# while idade >= 18:
+#     idade = int(input('Digite sua idade: '))
+# print('Sistema finalizado')
+# """
+# >=  -- maior igual
+# <= -- menor igual
+# -- maior
+# < -- menor
+# for = para/para cada
+# lista = [0, 6, 7, 10, 900] #lista de numeros
+# lista_nomes = ['Fernanda', 'Maria', 'Isadora', 'Rafael'] #lista textual
+
+# for numero in lista:
+#     numero += 1
+#     print(numero)
+
+# #for numero in lista: # para cada numero na lista
+#     print(numero) # apresente o numero
+
+# while idade > 18:
+#     idade = int(input('Digite sua idade: '))
+#     print('Sistema finalizado')
+
+# idade = int(input('Digite sua idade: '))
+# result_idade(idade)
+
+# nota = int(input("Digite uma nota de zero a dez: "))
+
+# while nota < 0 and nota > 10:
+#     nota = int(input("Digite a nota: "))
+# print("Valor válido ")
+
+# def qntd_notas(soma, quantidade, divisor):
+#     while quantidade > 0:
+#         n1 = int(input("Digite a nota: "))
+#         soma += n1
+#         quantidade -=1
+#         divisor += 1
+
+#     media = soma / divisor
+#     print(media)
+
+# quantidade = int(input("Digite a quantidade de notas: "))
+# soma = 0
+# divisor = 0
+# qntd_notas(soma, quantidade, divisor)
